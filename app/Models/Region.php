ddd400673a9c5be89510ca2b46f1145e93d57147
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2024-12-24 17:01:57 +0700

    add region model

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
+    protected $table = 'regions';
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'code',
+        'name'
+    ];
 }
